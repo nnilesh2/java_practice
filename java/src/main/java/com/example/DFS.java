efa225d60e5935e9f99063118a93d7aff3f8ea96
Nilesh Nagare <nnilesh2@gmail.com>
2024-07-14 12:03:13 +0000

    DFS algorithm

---
+++ java/src/main/java/com/example/DFS.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DFS {
+    public static void main(String[] args) {
+        // Graph representation using adjacency list
+        int[][] graph = { 
+            { 2, 3, 1 }, 
+            { 0 }, 
+            { 0, 4 }, 
+            { 0 }, 
+            { 2 } 
+        };
+
+        // Perform DFS starting from node 0
+        performDFS(graph, 0);
+    }
+
+    public static void performDFS(int[][] graph, int startNode) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        boolean[] isVisited = new boolean[graph.length];
+        
+        // Push the starting node onto the stack
+        stack.push(startNode);
+        
+        // Loop until the stack is empty
+        while (!stack.isEmpty()) {
+            int currentNode = stack.pop();
+            
+            // If the current node has not been visited
+            if (!isVisited[currentNode]) {
+                // Mark it as visited and print it
+                System.out.print(currentNode + " ");
+                isVisited[currentNode] = true;
+                
+                // Push all adjacent nodes onto the stack in reverse order
+                for (int i = graph[currentNode].length - 1; i >= 0; i--) {
+                    stack.push(graph[currentNode][i]);
+                }
+            }
+        }
+    }
+}
