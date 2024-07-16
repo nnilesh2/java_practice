c4c01f69ada4bcc19f082530ababcd73c4652f12
Nilesh Nagare <nnilesh2@gmail.com>
2024-07-16 10:11:12 +0000

    Added new files

---
+++ java/src/main/java/com/example/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,46 @@
+package com.example;
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+/*
+ Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice.
+The relative order of the elements should be kept the same.
+Since it is impossible to change the length of the array in some languages, 
+you must instead have the result be placed in the first part of the array nums. More formally, 
+if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. 
+It does not matter what you leave beyond the first k elements.
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+  
+ **/
+
+ public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,1,2,3,3};
+        int k = removeDuplicates(nums);
+        System.out.println("k = " + k);
+        System.out.print("nums = [");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i]);
+            if (i < k - 1) System.out.print(",");
+        }
+        System.out.println("]");
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) return nums.length;
+        
+        int index = 2; // Start from the 3rd element in the array
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) { // Compare current element with element at index-2
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
+
---
+++ java/src/main/java/com/example/RemoveElement.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.util.stream.IntStream;
+//https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+//Input: nums = [3,2,2,3], val = 3
+//Output: 2, nums = [2,2,_,_]
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int numToRemove = 2;
+
+        for(int i=0;i<nums.length;i++){
+            
+            if(nums[i]==numToRemove){
+                nums[i]=0;
+                shiftArray(nums,i);
+            }
+        }
+
+        IntStream.of(nums).forEach(System.out::print);
+    }
+
+    private static void shiftArray(int[] nums, int startIndex) {
+        
+        int temp;
+        for(int i=startIndex;i<nums.length;i++){
+           if(i+1<nums.length){
+            temp = nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
+           }
+            
+        }
+    }
+    
+}
